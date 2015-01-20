382f3b6faf7ec6351d29b6217e81ee7bbab860e6
Lars Tveito <larstvei@ifi.uio.no>
2015-01-20 01:33:45 +0100

    Reorganized, added sending updates and more.

--- shared-buffer-clients/shared-buffer-emacs/shared-buffer.el
+++ shared-buffer-clients/shared-buffer-emacs/shared-buffer.el
@@ -25,43 +25,107 @@
 
 ;;; Code:
 
+;;; Requirements
+
 (require 'websocket)
 (require 'cl-lib)
 (require 'json)
 
-(defvar sb-room nil
-  "A buffer that is shared belongs to a room. This variable keeps
-  an identifier to the room this buffer belongs to.")
+;;; Variables
 
-(defvar sb-host nil
+(defvar-local sb-room nil
+  "A buffer that is shared belongs to a room. This variable keeps an
+  identifier to the room this buffer belongs to.")
+
+(defvar-local sb-host nil
   "A buffer that is shared is connected to a server. This
   variable keeps the address of that server.")
 
-(defvar sb-socket nil
+(defvar-local sb-socket nil
   "A buffer that is shared is connected to a server. This
   variable keeps the connected socket.")
 
+;; Changing major-mode should not affect Shared Buffer.
+(dolist (var '(sb-room sb-host sb-socket))
+  (put var 'permanent-local t))
+
+;;; Socket communication
+
 (defun sb-connect-to-server (host &optional room)
   (setq sb-host "ws://localhost:8080")
-  (setq sb-socket (websocket-open
-                   sb-host
-                   :on-message 'sb-receive
-                   :on-open 'sb-on-open
-                   :on-close 'sb-on-close)))
+  (setq sb-socket (websocket-open sb-host
+                                  :on-message 'sb-receive
+                                  :on-open    'sb-on-open
+                                  :on-close   'sb-on-close))
+  (when sb-socket
+    (setf (process-buffer (websocket-conn sb-socket))
+          (current-buffer))))
 
 (defun sb-on-open (websocket)
-  (sb-send (json-encode (list :type 'room :room sb-room))))
+  (with-current-buffer (process-buffer (websocket-conn websocket))
+    (sb-send (json-encode (list :type 'room :room sb-room)))))
 
 (defun sb-on-close (websocket)
-  (message "Shared Buffer: Connection closed!"))
+  (with-current-buffer (process-buffer (websocket-conn websocket))
+    (message "Shared Buffer: Connection closed!")
+    (sb-quit)))
+
+;;; Send
+
+(defun sb-send (data)
+  (websocket-send-text sb-socket data))
+
+(defun sb-send-addition (point string)
+  (sb-send (json-encode (list :type'change
+                              :current-point (point)
+                              :change-point point
+                              :addition string))))
+
+(defun sb-send-deletion (point del)
+  (sb-send (json-encode (list :type'change
+                              :current-point (point)
+                              :change-point point
+                              :deletion del))))
+
+;;; Receive
+
+(defun sb-receive (websocket frame)
+  (with-current-buffer (process-buffer (websocket-conn websocket))
+    (let* ((payload (websocket-frame-payload frame))
+           (json-object-type 'plist)
+           (data (json-read-from-string payload))
+           (type (plist-get data :type)))
+      (print type)
+      (cond ((string= type "room")
+             (sb-set-room (plist-get data :room)))))))
+
+(defun sb-set-room (room)
+  (setq sb-room room)
+  (sb-add-key-to-kill-ring)
+  (message "Shared Buffer: \
+Connected to room: %s, the key was added to the kill ring." room))
+
+;;; Buffer modification
+
+(defun sb-after-change (beg end del)
+  "A function that is run after each change when
+`shared-buffer-mode' is enabled. Each change is sent to `sb-host'
+in order to keep the buffer synchronized."
+  (if (zerop del)
+      (sb-send-addition
+       beg (buffer-substring-no-properties beg end))
+    (sb-send-deletion beg del)))
+
+;;; Interactive functions
 
 (defun sb-share-buffer (host &optional buffer)
   (interactive "sHost: ")
   (with-current-buffer (or buffer (current-buffer))
-    (setf sb-socket (sb-connect-to-server host))
-    (if sb-socket
-        (message "Shared Buffer: Connected")
-      (message "Shared Buffer: Connection failed."))))
+    (sb-connect-to-server host)
+    (if (not sb-socket)
+        (message "Shared Buffer: Connection failed.")
+      (shared-buffer-mode 1)
+      (message "Shared Buffer: Connected"))))
 
 (defun sb-join-room (host room &optional buffer)
   (interactive "sHost: \nsRoom: ")
@@ -70,30 +134,32 @@
 
 (defun sb-disconnect (&optional buffer)
   (interactive)
-  (with-current-buffer (or buffer (current-buffer))
-    (websocket-close sb-socket)))
+  (when shared-buffer-mode
+    (with-current-buffer (or buffer (current-buffer))
+      (websocket-close sb-socket))
+    (sb-quit)))
 
-(defun sb-set-room (room)
-  (setq sb-room room)
-  (kill-new room)
-  (message "Shared Buffer: \
-Connected to room: %s, the key was added to the kill ring." room))
+(defun sb-add-key-to-kill-ring ()
+  (interactive)
+  (kill-new sb-room))
 
-(defun sb-receive (websocket frame)
-  (let* ((payload (websocket-frame-payload frame))
-         (json-object-type 'plist)
-         (data (json-read-from-string payload))
-         (type (plist-get data :type)))
-    (cond ((string= type "room")
-           (sb-set-room (plist-get data :room))))))
+;;; Minor mode
 
-(defun sb-send (data)
-  (websocket-send-text sb-socket data))
+(defun sb-quit ()
+  (dolist (var '(sb-room sb-host sb-socket))
+    (kill-local-variable var))
+  (shared-buffer-mode 0))
+
+(define-minor-mode shared-buffer-mode
+  "A minor mode for Shared Buffer."
+  nil " SB" nil
+  ;; fixme
+  (add-hook 'after-change-functions 'sb-after-change t t))
 
 ;; (sb-send (json-encode '(:type "update")))
 ;; (sb-disconnect)
 ;; (sb-share-buffer 'asdf)
-;; (process-list)
+;; (delete-process (car (process-list)))
 ;; sb-room
 
 
