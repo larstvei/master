311a33d008008ca8bbd57f1b52fb8aa1621f89ed
Lars Tveito <larstvei@ifi.uio.no>
2016-05-16 16:27:02 +0200

    Add events to history

--- shared-buffer-server/src/shared_buffer_server/core.clj
+++ shared-buffer-server/src/shared_buffer_server/core.clj
@@ -39,6 +39,35 @@
       (dissoc-in state [:rooms room])
       (update-in state [:rooms room :clients] disj client))))
 
+;;; Events
+
+(defn concurrent?
+  "Returns non-nil iff the events are concurrent."
+  [[o1 i1 m1 u1] [o2 i2 m2 u2]]
+  (and (empty? (intersection u1 u2))
+       (or (and (<= i1 i2) (<= i2 m1))
+           (and (<= i2 i1) (<= i1 m2)))))
+
+(defn precedes?
+  "Returns non-nil iff the e1 precedes e2."
+  [[o1 i1 m1 u1 :as e1] [o2 i2 m2 u2 :as e2]]
+  (if (concurrent? e1 e2)
+    (or (> (:pos o1) (:pos o2))
+        (and (= (:pos o1) (:pos o2)) (:del o1))
+        (and (= (:pos o1) (:pos o2))
+             (= (keys o1) (keys o2))
+             (< m1 m2)))
+    (< m1 m2)))
+
+(defn add-event
+  "Adds an event to the history."
+  [history [o i m u :as e1]]
+  (let [not-u? (comp not-empty (partial intersection u) last)
+        xs     (take-while not-u? history)
+        ys     (drop-while not-u? history)
+        [x y]  (split-with (partial precedes? e1) xs)]
+    (concat x [e1] y ys)))
+
 ;;; Receive
 
 (defmulti receive (comp keyword :type))
@@ -55,9 +84,8 @@
   (let [o (select-keys msg [:pos :ins :del])
         i (:state msg)
         m (get-in @state [:rooms (:key msg) :state])
-        u (get-in @state [:clients client :id])
-        event [o i m u]]
-    (println "event:" event)))
+        u #{(get-in @state [:clients client :id])}]
+    (swap! state update-in [:rooms (:key msg) :history] add-event [o i m u])))
 
 (defmethod receive :default [msg client]
   (println "default" msg))
