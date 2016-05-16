b474aaf1cb140d9dd4f6096dce79c5fe7f3dc465
Lars Tveito <larstvei@ifi.uio.no>
2016-05-16 16:35:25 +0200

    Refactored history and events to separate file

--- shared-buffer-server/src/shared_buffer_server/core.clj
+++ shared-buffer-server/src/shared_buffer_server/core.clj
@@ -3,6 +3,7 @@
   (:use org.httpkit.server)
   (:require [shared-buffer-server.app :refer :all])
   (:require [shared-buffer-server.utils :refer :all])
+  (:require [shared-buffer-server.history :refer :all])
   (:require [clojure.set :refer :all])
   (:require [clojure.data.json :as json]))
 
@@ -38,35 +39,6 @@
     (if (= 1 (count (get-in state [:rooms room :clients])))
       (dissoc-in state [:rooms room])
       (update-in state [:rooms room :clients] disj client))))
-
-;;; Events
-
-(defn concurrent?
-  "Returns non-nil iff the events are concurrent."
-  [[o1 i1 m1 u1] [o2 i2 m2 u2]]
-  (and (empty? (intersection u1 u2))
-       (or (and (<= i1 i2) (<= i2 m1))
-           (and (<= i2 i1) (<= i1 m2)))))
-
-(defn precedes?
-  "Returns non-nil iff the e1 precedes e2."
-  [[o1 i1 m1 u1 :as e1] [o2 i2 m2 u2 :as e2]]
-  (if (concurrent? e1 e2)
-    (or (> (:pos o1) (:pos o2))
-        (and (= (:pos o1) (:pos o2)) (:del o1))
-        (and (= (:pos o1) (:pos o2))
-             (= (keys o1) (keys o2))
-             (< m1 m2)))
-    (< m1 m2)))
-
-(defn add-event
-  "Adds an event to the history."
-  [history [o i m u :as e1]]
-  (let [not-u? (comp not-empty (partial intersection u) last)
-        xs     (take-while not-u? history)
-        ys     (drop-while not-u? history)
-        [x y]  (split-with (partial precedes? e1) xs)]
-    (concat x [e1] y ys)))
 
 ;;; Receive
 
