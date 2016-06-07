ef654c00b09c1f199089747cce40903e6fbe722d
Lars Tveito <larstvei@ifi.uio.no>
2016-06-07 14:10:43 +0200

    Simplify state extraction+always include user line

--- model/diffstates.py
+++ model/diffstates.py
@@ -4,9 +4,8 @@ from re import findall, DOTALL
 res = stdin.read()
 
 rules = findall(r'\,\'([\w-]+)', res)
-states = [map(lambda x: x.strip(), s.split('\n')) \
-          for s in findall(r'\{(.*?)\}', res, DOTALL)]
+states = [s.split('\n') for s in findall(r"\{(.*?)[-\w,']+\}", res, DOTALL)]
 
 for r, (s, t) in zip(rules, zip(states, states[1:])):
-    diff = [x for i,x in enumerate(t) if x is not '' and x != s[i]]
-    print '[' + r + '] :' + '\n\t' + '\n\t'.join(diff) + '\n'
+    diff = [x for i,x in enumerate(t) if x is not '' and x != s[i] or '< u' in x]
+    print '[' + r + '] :' + '\n' + '\n'.join(diff) + '\n'
