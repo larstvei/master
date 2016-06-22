28293d3443e171fa2141d25afcabcf0663cd8e3b
Lars Tveito <larstvei@ifi.uio.no>
2016-06-22 15:13:18 +0200

    Send appropriate response to incoming operations
    This commit adds the main functionality for responding to an incoming
    operation.

--- shared-buffer-server/src/shared_buffer_server/core.clj
+++ shared-buffer-server/src/shared_buffer_server/core.clj
@@ -4,6 +4,7 @@
   (:require [shared-buffer-server.app :refer :all])
   (:require [shared-buffer-server.utils :refer :all])
   (:require [shared-buffer-server.history :refer :all])
+  (:require [shared-buffer-server.syncprn :refer [syncprn]])
   (:require [clojure.data.json :as json]))
 
 (defonce state (atom {}))
@@ -58,6 +59,25 @@
     (-> state
         (update-client client seqno op (fn [_ x] (list x)))
         (update-room room history (map :(:clients room))))))
+
+;;; Send
+
+(defn make-msg [op seqno token]
+  {:type :operations
+   :operations (reverse op)
+   :seqno seqno
+   :token token})
+
+(defn send-op
+  "Sends an operation to a set of clients."
+  [op token clients]
+  (doseq [c clients]
+    (let [clinfo (get-in @state [:clients c])
+          seqno (:seqno clinfo)
+          msg (make-msg op seqno token)]
+      (send! c (json/write-str msg))
+      (swap! state update-client c (inc seqno) [op token] conj))))
+
 ;;; Receive
 
 (defmulti receive (comp keyword :type))
@@ -71,14 +91,23 @@
            (send! client)))))
 
 (defmethod receive :operation [msg client]
-  (let [op (select-keys msg [:pos :ins :del])
-        token (:token msg)
-        seqno (:seqno msg)
-        time (get-in @state [:rooms (:room msg) :token])
-        user #{(get-in @state [:clients client :id])}
-        event [op token time user]]
-    (swap! state update-in [:rooms (:room msg) :history] add-event event)
-    (prn op)))
+  (locking (get-in @state [:rooms (:room msg) :lock])
+    (let [room (get-in @state [:rooms (:room msg)])
+          cli  (get-in @state [:clients client])
+          op (list (select-keys msg [:pos :ins :del]))
+          seqno (+ (inc (:seqno msg)) (- (:seqno cli) (:seqno msg)))
+          time (inc (:token room))
+          event [op (:token msg) (:token room) #{(:id cli)}]
+          history (add-event (:history room) event)
+          op1 (make-op (:history room) history (:token msg))
+          op2 (make-response op op1 (:ops cli) (:token msg))
+          reply (make-msg op2 seqno time)
+          others (disj (:clients room) client)]
+      (syncprn "broadcast" op1)
+      (syncprn "reply" op2)
+      (send-op op1 time others)
+      (send! client (json/write-str reply))
+      (swap! state next-state client (inc seqno) [op2 time] history))))
 
 (defmethod receive :default [msg client]
   (println "default" msg))
@@ -87,12 +116,15 @@
 
 (defn reader [client]
   (fn [data]
-    (json/read-str data :key-fn keyword)))
+    (let [res (json/read-str data :key-fn keyword)]
+      (syncprn res)
+      res)))
 
 (defn handler
   "The handler for incoming requests."
   [req]
   (with-channel req channel
+    ;; TODO: make synchronous initialization.
     (if (websocket? channel)
       (swap! state initialize-client channel)
       (send! channel (app req)))
@@ -111,4 +143,4 @@
 (defn -main
   "The main function for Shared Buffer. It simply starts the server."
   [& args]                            ; <- is this right?
-  (swap! state assoc-in [:server] (run-server #'handler {:port 3705})))
+  (swap! state assoc :server (run-server #'handler {:port 3705})))
