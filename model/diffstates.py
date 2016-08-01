da1f2fd0bfc3e1d8c25a733d184e104243230043
Lars Tveito <larstvei@ifi.uio.no>
2016-08-01 14:46:42 +0200

    Comment diffstates

--- model/diffstates.py
+++ model/diffstates.py
@@ -1,11 +1,15 @@
 from sys import argv, stdin
 from re import findall, DOTALL
 
-res = stdin.read()
+content = stdin.read()
 
-rules = findall(r'\,\'([\w-]+)', res)
-states = [s.split('\n') for s in findall(r"\{(.*?)[-\w,']+\}", res, DOTALL)]
+# gather rule labels in a list
+rules = findall(r'\,\'([\w-]+)', content)
+# gather states in a list
+states = [s.split('\n') for s in findall(r"\{(.*?)[-\w,']+\}", content, DOTALL)]
 
+# iterate the states pairwise (along with the rule labels)
 for r, (s, t) in zip(rules, zip(states, states[1:])):
-    diff = [x for i,x in enumerate(t) if x is not '' and x != s[i] or '< u' in x]
+    # gather the lines from s and t, that differ
+    diff = [x for i, x in enumerate(t) if x is not '' and x != s[i]]
     print '[' + r + '] :' + '\n' + '\n'.join(diff) + '\n'
